ef2045257b235b7d8e6651e739fd86418565c8ea
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2021-02-23 08:25:18 +0100

    get initial test of permissions.py

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -2,5 +2,21 @@
 Tests for the workouts application.
 """
 from django.test import TestCase
+from workouts.models import Workout
 
 # Create your tests here.
+"""
+Tests for ./permissions.py
+"""
+class IsOwnerTestCase(TestCase):
+
+    def setUp(self):
+        #Workout.objects.create(name="testworkout")
+        #testworkout = Workout.objects.get
+        return super().setUp()
+
+    def test_has_object_permission(self):
+        self.assertEqual(True,True)
+    
+
+    
