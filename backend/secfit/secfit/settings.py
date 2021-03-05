a10b0c9ebfef7885a90014e15d7f56d5fc2d4f40
Tor Martin Frøberg Wang <tmwang@stud.ntnu.no>
2021-03-05 12:12:45 +0000

    Update CORS policy

--- backend/secfit/secfit/settings.py
+++ backend/secfit/secfit/settings.py
@@ -115,9 +115,10 @@ else:
 
 
 # CORS Policy
-CORS_ORIGIN_ALLOW_ALL = (
-    True
-)
+#CORS_ORIGIN_ALLOW_ALL = (
+#    True
+#)
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
