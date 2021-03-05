89e0a85c2c08df309b6e584c5288d9a94a6ebc66
Tobias Ørstad <tobiasio@ntnu.no>
2021-03-05 17:12:39 +0100

    Add boundary test

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -2,15 +2,27 @@
 Tests for the workouts application.
 """
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APIClient
+import json
 
 # Create your tests here.
-class TestTestCase(TestCase):
+class RegisterBoundaryTestCase(TestCase):
 
-    def test_true(self):
-        """Check if true is true"""
-        self.assertTrue(True)
+    def setUp(self):
+        self.request = json.loads('{"username": "","password": "123","password1": "123","athletes": [],"email": "","coach_files": [],"athlete_files": [],"workouts":[],"phone_number": "","country": "","city": "","street_address":""}')
+        self.client = APIClient()
 
+    def test_good_username(self):
+        self.request["username"] = "bob"
+        request = self.client.post('http://testserver/api/users/', json.dumps(self.request), content_type='application/json')
+        self.assertEquals(request.status_code,201)
+
+    def test_bad_username(self):
+        self.request["username"] = "<<<"
+        request = self.client.post('http://testserver/api/users/', json.dumps(self.request), content_type='application/json')
+        self.assertEquals(request.status_code,400)
         
-    def test_false(self):
-        """Check if false is false"""
-        self.assertFalse(False)
+        
+
+
+    
