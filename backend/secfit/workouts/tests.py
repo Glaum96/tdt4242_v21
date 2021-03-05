30665deff5d4f40f5074321380c9f4b045bf9955
Tobias Ørstad <tobiasio@ntnu.no>
2021-03-05 12:10:43 +0100

    Add tests to check if CI works

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -4,3 +4,13 @@ Tests for the workouts application.
 from django.test import TestCase
 
 # Create your tests here.
+class TestTestCase(TestCase):
+
+    def test_true(self):
+        """Animals that can speak are correctly identified"""
+        self.assertTrue(True)
+
+        
+    def test_false(self):
+        """Animals that can speak are correctly identified"""
+        self.assertFalse(True)
